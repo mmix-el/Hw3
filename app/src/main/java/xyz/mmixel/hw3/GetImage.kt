f609eef6a37e8af3f1031e8281ec64a570b01b02
mmix-el <mmix-el@yandex.ru>
2021-12-13 10:44:46 -0800

    Load with standard Android tools, without 3rd-party libraries.

--- app/src/main/java/xyz/mmixel/hw3/GetImage.kt
+++ app/src/main/java/xyz/mmixel/hw3/GetImage.kt
@@ -1,5 +1,7 @@
 package xyz.mmixel.hw3
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -10,6 +12,10 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
+
 
 private const val TAG = "GetImage"
 
@@ -21,24 +27,27 @@ class GetImage : AppCompatActivity() {
         setContentView(R.layout.activity_get_image)
         imageView = findViewById(R.id.imageView)
 
-        findViewById<EditText>(R.id.imageUriForPicasso).apply {
+        findViewById<EditText>(R.id.imageUriForAndroid).apply {
             setOnKeyListener { _, keyCode, event ->
                 if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
-                    Picasso.get()
-                        .load(Uri.parse(this.text.toString()))
-                        .into(imageView, object : Callback {
-                            override fun onSuccess() {
-                                Log.d(TAG, "Done.")
+                    thread {
+                        try {
+                            val stream = URL(this.text.toString()).openStream()
+                            val bitmap: Bitmap = BitmapFactory.decodeStream(stream)
+                            runOnUiThread {
+                                imageView.setImageBitmap(bitmap)
                             }
-
-                            override fun onError(e: Exception?) {
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                            runOnUiThread {
                                 Toast.makeText(
                                     this@GetImage,
-                                    "Error. ${e?.message}",
+                                    "Error. ${e.message}",
                                     Toast.LENGTH_SHORT
                                 ).show()
                             }
-                        })
+                        }
+                    }
                     return@setOnKeyListener true
                 }
                 false
